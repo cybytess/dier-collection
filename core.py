3378decba1ba41d18fc8cf8f8afafbaf553afbce
cybytess <87681509+cybytess@users.noreply.github.com>
2021-10-10 16:54:40 +0800

    构建DIER Collection 0.1
    已完成读取功能

--- core.py
+++ core.py
@@ -1,2 +1,22 @@
-def read():
-    
+def load(file):
+    content = open(file, encoding='utf-8', mode='r')
+    content = content.readlines()
+    return content
+
+
+def read(file, mode):
+    if mode == "title" or mode == "t":
+        content = file[5].split('::')[1]
+        return content
+    if mode == "location" or mode == "l":
+        content = file[4].split('::')[1]
+        return content
+    if mode == "start" or mode == "s":
+        content = file[2].split('::')[1]
+        return content
+    if mode == "end" or mode == "e":
+        content = file[3].split('::')[1]
+        return content
+
+
+#print(read(load("bookmarks001.SCA"), "l"))
