46f55b40c402f7253d44b7a6559ea4e514b5e4b0
cybytess <87681509+cybytess@users.noreply.github.com>
2022-01-21 01:30:39 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="lindaxy",
+    version="0.0.1",
+    author="cybytess",
+    author_email="p2002817@outlook.com",
+    description="Collection file editor for DIER",
+
+    url="https://github.com/cybytess/lindaxy", 
+
+    packages=find_packages(),
+    
+    classifiers = [
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+
+        'Intended Audience :: Customer Service',
+
+        ]
+    
+    
+)
